e186e4a6cbcd71ab23c30048012a518cd5c74b95
Mike Saunders <m_b_saunders@msn.com>
2014-07-06 22:30:19 -0700

    Hello2 initial commit

---
+++ Hello2.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   Hello2.cpp
+ * Author: Mike
+ *
+ * Created on April 6, 2014, 9:14 PM
+ */
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv) {
+    cout <<"Hello There!  This is a test...."<<endl;
+    return 0;
+}
+
---
+++ build/Debug/MinGW-Windows/Hello2.o.d
@@ -0,0 +1,1 @@
+build/Debug/MinGW-Windows/Hello2.o: Hello2.cpp
